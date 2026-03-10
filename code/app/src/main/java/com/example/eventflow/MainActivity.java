d0ab29696ed7fe4eeb83e8ddea0677f1a3ff1b1f
natashakk0602 <nkrishn2@ualberta.ca>
2026-03-10 16:26:07 -0600

    Implemented waiting list feature and tests

--- code/app/src/main/java/com/example/eventflow/MainActivity.java
+++ code/app/src/main/java/com/example/eventflow/MainActivity.java
@@ -8,17 +8,37 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.content.Intent;
+import android.widget.Button;
+/**
+ * MainActivity
+ * This activity displays the event details screen.
+ * It contains a button that allows the organizer to navigate
+ * to the waiting list screen.
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button button = findViewById(R.id.viewWaitingListButton);
+
+        button.setOnClickListener(v -> {
+
+            Intent intent = new Intent(MainActivity.this, WaitingListActivity.class);
+            startActivity(intent);
+
         });
     }
 }
