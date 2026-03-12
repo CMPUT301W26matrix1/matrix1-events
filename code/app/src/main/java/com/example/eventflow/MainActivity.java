b7b304e18707efb9bd187374cb2aab114d5cd9d8
Mahisruthi <mogalla@ualberta.com>
2026-03-12 12:07:04 -0600

    notification feature complete

--- code/app/src/main/java/com/example/eventflow/MainActivity.java
+++ code/app/src/main/java/com/example/eventflow/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.eventflow;
 
 import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -9,6 +11,7 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +20,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        Button sendButton = findViewById(R.id.sendNotificationButton);
+
+        sendButton.setOnClickListener(v -> {
+
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+            String message = "You've been selected!";
+            String eventName = "Sample Event";
+            String details = "Check event details.";
+
+            String testUserId = "VLSSOuGA27beNDV7N3sB8lh7O1q2";
+
+            Notification notification = new Notification(message, eventName, details);
+
+            db.collection("users")
+                    .document(testUserId)
+                    .collection("notifications")
+                    .add(notification);
+        });
         FirebaseAuth auth = FirebaseAuth.getInstance();
 
         auth.signInWithEmailAndPassword("abc@test.com", "123456")
